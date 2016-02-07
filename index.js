175e0cc1a370a2bf8b96f6d51e95c45805c094b3
Michael Perrotte <mike@mikecorp.ca>
2016-02-06 21:24:22 -0500

    project index file

---
+++ index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// INFO: base Collins requirements
+const Collins = require('./libs/Collins');
+const config = require('./config');
+
+// INFO: create new instance of Collins
+let collins = new Collins(config);
