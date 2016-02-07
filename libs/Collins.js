f4c45114953d912c4aca4b8a21427ddf7f622d80
Michael Perrotte <mike@mikecorp.ca>
2016-02-07 02:16:45 -0500

    created control flow in Collins class

--- libs/Collins.js
+++ libs/Collins.js
@@ -6,17 +6,50 @@
 
 'use strict';
 
-class Collins {
-  constructor(config) {
+// INFO: collins specific modules
+const CollinsError = require('./CollinsError');
+const Loader = require('../utils/Loader');
 
+// INFO: common modules
+const async = require('async');
+// const Emitter = require('events');
+const Emitter = require('eventemitter2');
+
+class Collins extends Emitter.EventEmitter2 {
+  constructor(config) {
+    super({
+      wildcard: true,
+      delimiter: ':'
+    });
+
+    // TODO: check if config is valid
+    this.config = Loader.validateConfig(config);
     this.services = [];
     this.Runtime = require('../utils/Runtime');
-    // TODO: check if config is valid
   }
 
-  use(serviceGear) {
+  use(service_gear) {
+    this.services.push(service_gear);
+  }
 
-    // TODO: add service to array of services
+  start() {
+
+    // TODO: call the Loader
+    async.series([
+      Loader.init.bind(this),
+      Loader.initConfig.bind(this),
+      Loader.initServices.bind(this),
+      Loader.connectServices.bind(this),
+      Loader.initServiceCogs.bind(this),
+      Loader.initActions.bind(this)
+    ], (error, results) => {
+
+      // INFO: we got an error from some init loader
+      if (error) {
+        this.emit('error:start', error);
+      }
+      // INFO: all the initializations have been completed
+    });
   }
 }
 
