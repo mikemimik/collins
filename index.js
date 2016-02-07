5a428713362c857f11d38bdff68db4c09e86f9ea
Michael Perrotte <mike@mikecorp.ca>
2016-02-07 02:17:00 -0500

    updated utilization of Collins module

--- index.js
+++ index.js
@@ -2,7 +2,21 @@
 
 // INFO: base Collins requirements
 const Collins = require('./libs/Collins');
-const config = require('./config');
+const config = require('./configs');
+
+// INFO: Collins service-gear
+const mumbleGear = require('collins-mumble');
 
 // INFO: create new instance of Collins
 let collins = new Collins(config);
+
+collins.use(mumbleGear);
+
+// INFO: no arrow functions (need context intact)
+collins.on('error:*', function(error) {
+  // console.log('>>', 'EVENT', this.event); // TESTING
+  // console.log('>>', 'THIS:', this); // TESTING
+  console.log('>>', 'Error', error);
+});
+
+collins.start();
