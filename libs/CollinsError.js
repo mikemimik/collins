b3c461641b79d47b8f374528c27d6cc236a983fb
Michael Perrotte <mike@mikecorp.ca>
2016-02-07 02:16:08 -0500

    added CollinsError class code

---
+++ libs/CollinsError.js
@@ -0,0 +1,33 @@
+'use strict';
+
+class ExtendableError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = this.constructor.name;
+    this.message = message;
+    Error.captureStackTrace(this, this.constructor.name);
+  }
+}
+
+/**
+ * @summary Error class for delivering errors with Collins
+ *
+ * @param {String} type Error type name
+ * @param {Object} data Data given to the error
+ */
+class CollinsError extends ExtendableError {
+  constructor(type, data) {
+    super('constructor');
+    this.data = data;
+    this.message = '\'' + type + '\' error message received';
+    let reason = data.details || data.reason;
+    if (reason) {
+      this.message += ':\n"' + reason + '"\n';
+    } else {
+      this.message += '. ';
+    }
+    this.message += 'See \'data\' for details.';
+  }
+}
+
+module.exports = CollinsError;
