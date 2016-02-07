71d517f0de26bfb35faabd97771741898dde5dd8
Michael Perrotte <mike@mikecorp.ca>
2016-02-06 21:24:54 -0500

    Initial configuration of project source code

---
+++ libs/Collins.js
@@ -0,0 +1,23 @@
+/**
+ * Collins Module
+ * @module collins
+ * @see module:collins
+ */
+
+'use strict';
+
+class Collins {
+  constructor(config) {
+
+    this.services = [];
+    this.Runtime = require('../utils/Runtime');
+    // TODO: check if config is valid
+  }
+
+  use(serviceGear) {
+
+    // TODO: add service to array of services
+  }
+}
+
+module.exports = Collins;
---
+++ utils/Loader.js
@@ -0,0 +1,9 @@
+'use strict';
+
+class Loader {
+  static init(next) {}
+  static initConfig(next) {}
+  static initServices(next) {}
+  static initServiceCogs(next) {}
+  static initActions(next) {}
+}
