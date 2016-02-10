68694ded7e456098ee750b39608dcc6a857fd6c2
Michael Perrotte <mike@mikecorp.ca>
2016-02-10 13:40:07 -0500

    added small test for loader.initServices

---
+++ tests/Loader.test.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const assert = require('assert');
+const Loader = require('../utils/Loader');
+
+/**
+ * Test to see if Loader.initServices parces the config files correctly
+ */
+
+const mockCollins = {
+  services: [ { name: 'CollinsMumble' }, { name: 'CollinsSlack' } ]
+};
+
+describe('Loader', () => {
+  describe('initServices', () => {
+    it('should parse config files correctly', (done) => {
+      Loader.initServices(done);
+    });
+  });
+});
