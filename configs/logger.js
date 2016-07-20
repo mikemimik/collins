89c41daa899a8b30648f92bcc601e6d5a575c60e
afrolionx <mike@mikecorp.ca>
2016-07-20 02:53:24 -0400

    refactored logger configuration

--- configs/logger.js
+++ configs/logger.js
@@ -1,39 +1,42 @@
 'use strict';
 
 const Winston = require('winston');
+
 const CollinsDefault = new Winston.transports.Console({
   colorize: true,
   prettyPrint: true,
   depth: 1
 });
 
-exports.config = {
-  levels: {
-    error: 0,
-    warn: 1,
-    info: 2,
-    core: 3,
-    gear: 4,
-    verbose: 5,
-    debug: 6
+const logLevels = {
+  core: {
+    error: 0, warn: 1, info: 2,
+    core: 3, gear: 4,
+    verbose: 5, debug: 6
   },
-  colors: {
-    error: 'red',
-    debug: 'blue',
-    warn: 'yellow',
-    verbose: 'grey',
-    info: 'green',
-    core: 'cyan',
-    gear: 'magenta'
+  gear: {
+    error: 0, warn: 1, info: 2,
+    cog: 3
+    verbose: 4, debug: 5
   }
 };
 
-exports.filter = function(level, msg, meta) {
+const logColors = {
+  error: 'red', warn: 'yellow', info: 'green',
+  core: 'cyan', gear: 'magenta', cog: 'magenta',
+  verbose: 'grey', debug: 'blue'
+};
+
+const loggingOutputFilter = function (level, msg, meta) {
   return this.constructor.name
     + ' > '
     + msg;
 };
 
-exports.transports = [
-  CollinsDefault
-];
+module.exports = {
+  filter: loggingOutputFilter,
+  transports: [ CollinsDefault ],
+  logLevels: logLevels,
+  logColors: logColors,
+  Init: Winston.Logger
+};
