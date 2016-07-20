6f8ba7de291a9eb05092ca944ecabc4c24eac91f
afrolionx <mike@mikecorp.ca>
2016-07-19 22:58:27 -0400

    refactored CollinsError class

--- libs/CollinsError.js
+++ libs/CollinsError.js
@@ -1,41 +1,45 @@
 'use strict';
 
-class ExtendableError extends Error {
-  constructor(message) {
-    super(message);
-    this.name = this.constructor.name;
-    this.message = message;
-    Error.captureStackTrace(this, this.constructor.name);
-  }
-}
-
 /**
  * @summary Error class for delivering errors with Collins
  *
  * @param {String} type Error type name
  * @param {Object} data Data given to the error
  */
-class CollinsError extends ExtendableError {
+class CollinsError extends Error {
   constructor(type, data) {
-    super(type);
+    super();
+    this.name = this.construtor.name;
     this.data = data || {};
     this.type = type;
-    this.message = '\'' + type + '\' error message received';
+    this.message = `<${this.type}> error message received`;
     let reasons = data.details || data.reasons;
     if (reasons) {
-      this.message += ':';
+      this.message += ': ';
+
+      // INFO: check if `reasons` is an array
       if (Array.isArray(reasons)) {
-        reasons.forEach((reason) => {
-          this.message += '\n\t"' + reason + '"';
+        reasons.forEach((reason, index) => {
+          this.message += `${index+1}> ${reason} `;
         });
       } else {
-        this.message += '\n"' + reasons + '"\n';
+        this.message += `${reasons}`;
       }
     } else {
-      this.message += '. ';
+      this.message += '.';
     }
     this.message += '\nSee \'data\' for details.';
   }
 }
 
 module.exports = CollinsError;
+
+/**
+ * @summary Enum of possible errors to create
+ */
+ /**
+  * FileReadError => error:FileRead
+  * ConfigError => error:config
+  *             => error:configure
+  *
+  */
