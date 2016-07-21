013a6ec79891fc76fb4d64f9ce67611484106104
afrolionx <mike@mikecorp.ca>
2016-07-21 13:59:04 -0400

    refactored config module/dir

--- configs/index.js
+++ configs/index.js
@@ -1,8 +1,24 @@
 'use strict';
 
+const convict = require('convict');
+const schema = require('./schema');
+
+convict.addFormat({
+  name: 'string-nospace',
+  validate: function validateType (val) {
+    if (typeof val === 'string') {
+      if (val.split(' ').length > 1) {
+        throw new Error('must not contain spaces');
+      }
+    } else {
+      throw new Error('must be a string');
+    }
+  }
+});
+
+const config = convict(schema);
+
 module.exports = {
-  name: 'Collins',
-  userAgent: 'collins',
-  logLevel: 'debug',
-  debug: true
+  logger: require('./logger'),
+  tools: config
 };
---
+++ configs/schema.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const logger = require('./logger');
+
+let defaultSchema = {
+  name: {
+    doc: 'Name of this instance of the application',
+    format: 'string-nospace',
+    default: 'Collins'
+  },
+  userAgent: {
+    doc: 'User Agent to use while accessing networks',
+    format: 'string-nospace',
+    default: 'collins'
+  },
+  logLevel: {
+    doc: 'Level at which to log events and errors',
+    format: Object.keys(logger.logLevels.core);
+    default: 'debug'
+  }
+};
+
+module.exports = defaultSchema;
