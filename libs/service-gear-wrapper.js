064e11dac2cb6b9b79775014a0b820b2aecab7fa
afrolionx <mike@mikecorp.ca>
2016-08-31 12:15:27 -0400

    added used property to constructor

--- libs/service-gear-wrapper.js
+++ libs/service-gear-wrapper.js
@@ -8,6 +8,7 @@ class ServiceGearWrapper {
       throw new CollinsError('invalid function call');
     }
     this.logger = null;
+    this.logLevel = null;
     this.Creator = gear;
     this.instance = null;
     this.config = null;
